592ceed2a57725ab351c5d0c8ab4c2a8cac4ad31
Ashish Pathak <ashishpathakavs@gmail.com>
2024-10-02 19:49:45 +0530

    Add files via upload

--- sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
 
     @Id
@@ -27,4 +30,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
